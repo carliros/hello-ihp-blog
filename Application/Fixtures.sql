f70826d7a63ee6ee8af289e85f820929c1a37339
Carlos Gomez <carliros.g@gmail.com>
2021-04-21 15:18:37 -0400

    Add created_at column to posts table

--- Application/Fixtures.sql
+++ Application/Fixtures.sql
@@ -12,3 +12,14 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 
+SET SESSION AUTHORIZATION DEFAULT;
+
+ALTER TABLE public.posts DISABLE TRIGGER ALL;
+
+INSERT INTO public.posts (id, title, body) VALUES ('3e76f707-11af-49af-a7ca-89353c40081b', 'Cine foro', 'This is an event for watching "Los del camino" film from Colombia.');
+INSERT INTO public.posts (id, title, body) VALUES ('6c26933f-47f8-4a07-8ffc-5d8709a9d8cf', 'Hello', 'World');
+
+
+ALTER TABLE public.posts ENABLE TRIGGER ALL;
+
+
